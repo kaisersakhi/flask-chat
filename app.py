ab6b582618f79d18a558158d5208ca60262115b9
kaisersakhi <kaisersakhi@gmail.com>
2022-08-02 15:40:52 +0530

    added

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def home():
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, )
