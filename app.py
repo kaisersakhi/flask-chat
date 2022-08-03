5e67a576a5569db06918d3577fb7b01b32e938a8
kaisersakhi <kaisersakhi@gmail.com>
2022-08-03 18:21:15 +0530

    modified app and index

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, emit
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,11 +13,23 @@ def home():
     return "<h1>Start Fund - Chat App</h1>"
 
 
-@socketio.on('message')  # here the 'message' will specify
-def handle_message(msg):  # will get called on each receive
+@socketio.on('on_connect')
+def on_connect(data):
+    # print(data)
+    send(data) #broadcasting new username
+
+
+# @socketio.on('message')  # here the 'message' will specify
+# def handle_message(msg):  # will get called on each receive
+#     print(msg)
+#     # send(msg, broadcast=True)  # get the message and broadcast it back to every client connected
+
+
+@socketio.on("message")
+def my_message(msg):
     print(msg)
-    send(msg, broadcast=True)  # get the message and broadcast it back to every client connected
+    send(msg, broadcast = True) #this will call js 
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
